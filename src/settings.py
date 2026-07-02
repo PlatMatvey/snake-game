eedf3a4098fef1561744d2adb46a7af0224995ca
PlatMatvey <matvey0910008@email.com>
2026-07-02 17:56:13 +0600

    Add game settings

---
+++ src/settings.py
@@ -0,0 +1,13 @@
+# Размеры экрана
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 800
+BLOCK = 40
+
+# Скорость игры
+FPS = 7
+
+# Цвета
+COLOR_BG = (0, 0, 0)
+COLOR_SNAKE = (255, 255, 0)
+COLOR_FOOD = (220, 20, 60)
+COLOR_TEXT = (255, 255, 255)
